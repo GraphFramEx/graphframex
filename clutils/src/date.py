63318d30e1029a2cea72deb7f6b76bd008ffde72
kamara <22amara.kenza@gmail.com>
2022-06-04 16:14:18 +0200

    gitignore

--- clutils/src/date.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-class GMT1(datetime.tzinfo):
-    def utcoffset(self, dt):
-        return datetime.timedelta(hours=1)
-    def dst(self, dt):
-        return datetime.timedelta(0)
-    def tzname(self,dt):
-        return "Europe/Paris"
